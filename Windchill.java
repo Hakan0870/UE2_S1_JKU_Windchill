d3b9a2c3e84d5479f236852d00af8a41797aa69e
Hakan Ucar <hakan0870@gmail.com>
2021-07-20 09:36:23 +0200

    Initial commit

---
+++ Windchill.java
@@ -0,0 +1,14 @@
+class Windchill{
+    public static void main(String[] args) {
+        Out.print("Wind speed in km/h: ");
+        double v = In.readDouble();
+
+        Out.print("Temperature in Celsius: ");
+        double t = In.readDouble();
+
+        double wct = 13.12 + 0.6215 * t + (0.3965 * t - 11.37) * Math.pow(v, 0.16);
+        int round = (int)(wct * 100);
+        Out.print("Windchill-Temperature: " + (int)wct + "." + round / 10 % 10 + "" + round % 10);
+    }
+
+}
