fbf3dc0783f00a70a9432680b1b0c8b379e2938b
Hakan Ucar <hakan0870@gmail.com>
2021-07-20 09:48:54 +0200

    format integer

--- Windchill.java
+++ Windchill.java
@@ -7,8 +7,9 @@ class Windchill{
         double t = In.readDouble();
 
         double wct = 13.12 + 0.6215 * t + (0.3965 * t - 11.37) * Math.pow(v, 0.16);
-        int round = (int)(wct * 100);
-        Out.print("Windchill-Temperature: " + (int)wct + "." + round / 10 % 10 + "" + round % 10);
+        /*int round = (int)(wct * 100);
+        Out.print("Windchill-Temperature: " + (int)wct + "." + round / 10 % 10 + "" + round % 10);*/
+        System.out.format("Windchill-Temperature: %.2f%n", wct); //easier way to format integer
     }
 
 }
